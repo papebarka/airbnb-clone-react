a887e79338381ba08445ccbfc61a566f60587f20
Pape Barka Seydou Coulibaly <papebarka@gmail.com>
2020-07-18 02:08:07 +0000

    Introduction to React - Airbnb Clone | LeWagon

---
+++ src/components/hello.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Hello extends React.Component {
+
+  render(){
+    // Statement
+    return <div>Hello {this.props.name}!</div>;
+  }
+}
+
+export default Hello;
